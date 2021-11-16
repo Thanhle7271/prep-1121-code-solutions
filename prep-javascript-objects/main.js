7a6c8f99f7bd8209e981a3cc76fc659089f51095
Thanh t le <Tommytongle@gmail.com>
2021-11-16 14:49:34 -0800

    completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,16 @@
+var person = {
+  firstname: "Thanh",
+  Lastname: "Le",
+  hobby: "gym",
+}
+console.log(person);
+
+console.log("The person's name is:",person.firstname, person.Lastname);
+
+person.job = "jerk";
+console.log("The person's current job is: ",person.job);
+
+person.previousJob = "amigo" ;
+console.log("The person's previous job is:", person.previousJob);
+
+console.log("The complete person object:",person)
