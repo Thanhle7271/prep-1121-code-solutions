69a5064dacf89daba8a7aa735d7f8b2596d3d24c
Thanh t le <Tommytongle@gmail.com>
2021-11-16 15:50:08 -0800

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,17 @@
+const languages = [
+  "html",
+  "css",
+  "javascript",
+  "react",
+  "node",
+  "postgres",
+];
+console.log(languages);
+var firstelement = languages [0];
+console.log("The first element of the array is", firstelement);
+var thirdelement = languages [2];
+console.log("The 3rd element of the arry is", thirdelement);
+var length =languages.length;
+console.log("The length of the array is", length);
+var lastelement = languages[languages.length -1];
+console.log("The last element of the array is",lastelement);
