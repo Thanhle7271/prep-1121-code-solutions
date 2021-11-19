7b1f65ae6be355d0002d024f2817a45d232f905c
Thanh t le <Tommytongle@gmail.com>
2021-11-18 16:26:02 -0800

    completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,61 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+var sum = addTwoNumbers(2, 2);
+console.log('sum', sum);
+
+
+function converthourstominutes(hours) {
+  return hours * 60;
+}
+var minutes= converthourstominutes(2);
+console.log('minutes',minutes);
+
+
+function getgreeting(x) {
+  return x;
+}
+var Hello = getgreeting('world')
+console.log('hello', Hello)
+
+
+function multipleanddivideby5(x,y) {
+  return (x*y)/5;
+}
+var sum = multipleanddivideby5(10, 5);
+console.log('sum', sum);
+
+
+function substracttwonumbers(x, y) {
+  return x - y;
+}
+var sum = substracttwonumbers(10, 4);
+console.log('sum', sum);
+
+
+function getcirclecircumference(x) {
+  return 2*Math.PI*x;
+}
+var sum = getcirclecircumference(4);
+console.log('sum', sum);
+
+
+function getfullname(x, y) {
+  return x + " " + y;
+}
+var sum = getfullname("thanh", "le");
+console.log('sum', sum);
+
+
+function cube(x) {
+  return x*x*x
+}
+var sum = cube(5);
+console.log('sum', sum);
